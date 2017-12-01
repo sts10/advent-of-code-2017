9ffc6af66ee8b9c4ade2d2c12bbd524b71896e5c
sts10 <sschlinkert@gmail.com>
2017-12-01 17:30:04 -0500

    day 1 part 1 works

--- day01-rust/src/main.rs
+++ day01-rust/src/main.rs
@@ -1,26 +1,33 @@
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
     println!("Let's do this");
-    let input = "1122";
-    println!("{}", input);
-    let mut array_chars: [u32; 4] = [0; 4];
+    // read input from txt file
+    let mut f = File::open("input1.txt").expect("file not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    let first_digit = contents.chars().nth(0).unwrap().to_digit(10).unwrap();
     let mut i: usize = 0;
     
-    // build array_chars
-    for c in input.chars() { 
-        // println!("{:?}", c.to_digit(10));
-        array_chars[i] = c.to_digit(10).unwrap();
-        i = i + 1;
-    }
-    println!("{:?}", array_chars);
-
     let mut sum: u32 = 0;
-    i = 0;
-    for _ in array_chars.iter() {
-        if i+1 < array_chars.len() && array_chars[i] == array_chars[i+1]{
-            sum = sum + array_chars[i];
+    let mut before_digit: u32 = 0;
+    for c in contents.trim().chars() { 
+        let this_digit = c.to_digit(10).unwrap();
+
+        if i+1 < contents.chars().count() && this_digit == before_digit{
+            sum = sum + this_digit;
         }
+
+        if i + 1 == contents.chars().count() - 1 && this_digit == first_digit{
+            println!("this doesn't run, right?");
+            sum = sum + this_digit;
+        }
+        before_digit = this_digit;
         i = i + 1;
     }
-
-    println!("{}", sum);
+    println!("sum is {}", sum);
 }
