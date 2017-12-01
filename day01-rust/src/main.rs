f96d76b4662f7c5bb975ad209b7ae929af508a05
sts10 <sschlinkert@gmail.com>
2017-12-01 18:05:35 -0500

    part 1 day 1 refactored

--- day01-rust/src/main.rs
+++ day01-rust/src/main.rs
@@ -4,16 +4,16 @@ use std::io::prelude::*;
 fn main() {
     // read input from txt file
     let mut f = File::open("input1.txt").expect("file not found");
-
     let mut contents = String::new();
     f.read_to_string(&mut contents)
         .expect("something went wrong reading the file");
 
-    let first_digit = contents.chars().nth(0).unwrap().to_digit(10).unwrap();
     let mut i: usize = 1;
-    
     let mut sum: u32 = 0;
-    let mut before_digit: u32 = 0;
+    // initialize before_digit to the last digit of contents
+    let contents_size = contents.chars().count();
+    let mut before_digit: u32 = contents.trim().chars().nth(contents_size-2).unwrap().to_digit(10).unwrap();
+
     for c in contents.trim().chars() { 
         let this_digit = c.to_digit(10).unwrap();
 
@@ -21,10 +21,6 @@ fn main() {
             sum = sum + this_digit;
         }
 
-        // if at the end onf the list, check against first digit
-        if i == contents.chars().count() - 1 && this_digit == first_digit{
-            sum = sum + this_digit;
-        }
         before_digit = this_digit;
         i = i + 1;
     }
