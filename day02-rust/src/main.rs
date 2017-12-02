d50669f22416ca732f204e38ae7a0f4766acc8f5
sts10 <sschlinkert@gmail.com>
2017-12-02 09:42:50 -0500

    adds a comment

--- day02-rust/src/main.rs
+++ day02-rust/src/main.rs
@@ -6,29 +6,24 @@ use std::fs::File;
 
 fn main() {
     let mut sum: usize = 0;
-    // read input from txt file 
     let file_name = "input.tsv";
-    // let file_name = "test.tsv";
     let f = File::open(file_name).unwrap();
     let file = BufReader::new(&f);
     for line in file.lines() {
         let l = line.unwrap();
         println!("{}", l); 
         let str_vec: Vec<&str>= l.split("	").collect();
-        // let this_vec: Vec<usize>= l.split("	").filter_map(|s| s.to_digit(10)).collect();
-
+        // convert to vector of digits
         let mut d_vec: Vec<usize> = [].to_vec();
         for s in str_vec {
             d_vec.push(s.parse::<usize>().unwrap());
         }
-        // let data: Vec<u32> = l.spl.filter_map(|s| s.to_digit(10)).collect();
-        // find min + max
-        // add it to sum
+
         let v_max = d_vec.iter().max().unwrap();
         let v_min = d_vec.iter().min().unwrap();
         sum = v_max - v_min + sum;
-
     }          
-    // let contents: Vec<u32> = file_to_vec(file_name);
     println!("sum is {}", sum);
 }
+
+// my part 1 correct answer is 21845
