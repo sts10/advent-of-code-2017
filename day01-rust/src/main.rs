f52534901cfb4e273bed43f42c111a1a13b0e46d
sts10 <sschlinkert@gmail.com>
2017-12-01 21:20:21 -0500

    adds a bunch of tests to the bottom of main.rs

--- day01-rust/src/main.rs
+++ day01-rust/src/main.rs
@@ -51,3 +51,63 @@ fn file_to_vec(filename: &str) -> (Vec<u32>){
 
     return data;
 }
+
+
+// 1122 produces a sum of 3 (1 + 2) because the first digit (1) matches the second digit and the
+// third digit (2) matches the fourth digit.
+#[test]
+fn part_1_example1() {
+    // let contents = vec![1,1,2,2];
+    // assert_eq!(part_1(&contents), 3);
+    assert_eq!(part_1(&vec![1,1,2,2]), 3);
+}
+
+// 1111 produces 4 because each digit (all 1) matches the next.
+#[test]
+fn part_1_example2() {
+    assert_eq!(part_1(&vec![1,1,1,1]), 4);
+}
+
+// 1234 produces 0 because no digit matches the next.
+#[test]
+fn part_1_example3() {
+    assert_eq!(part_1(&vec![1,2,3,4]), 0);
+}
+
+// 91212129 produces 9 because the only digit that matches the next one is the last digit, 9.
+#[test]
+fn part_1_example4() {
+    assert_eq!(part_1(&vec![9,1,2,1,2,1,2,9]), 9);
+}
+
+
+
+// 1212 produces 6: the list contains 4 items, and all four digits match the digit 2 items ahead.
+#[test]
+fn part_2_example1() {
+    assert_eq!(part_2(&vec![1,2,1,2]), 6);
+}
+
+// 1221 produces 0, because every comparison is between a 1 and a 2.
+#[test]
+fn part_2_example2() {
+    assert_eq!(part_2(&vec![1,2,2,1]), 0);
+}
+
+// 123425 produces 4, because both 2s match each other, but no other digit has a match.
+#[test]
+fn part_2_example3() {
+    assert_eq!(part_2(&vec![1,2,3,4,2,5]), 4);
+}
+
+// 123123 produces 12.
+#[test]
+fn part_2_example4() {
+    assert_eq!(part_2(&vec![1,2,3,1,2,3]), 12);
+}
+
+// 12131415 produces 4
+#[test]
+fn part_2_example5() {
+    assert_eq!(part_2(&vec![1,2,1,3,1,4,1,5]), 4);
+}
