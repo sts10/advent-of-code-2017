163aac83840e361df473e201bdefb264d47e9a03
sts10 <sschlinkert@gmail.com>
2017-12-04 19:17:10 -0500

    gives correct answer for part 1

--- day03-rust/src/main.rs
+++ day03-rust/src/main.rs
@@ -1,16 +1,16 @@
 
 fn main() {
     println!("Ring number for 12 is {}", get_ring_number(12));
-    println!("Ring number for 20 is {}", get_ring_number(20));
-    println!("Ring number for 28 is {}", get_ring_number(28));
-    println!("Ring number for 46 is {}", get_ring_number(46));
-    println!("-----");
-    println!("distance to closest railraod for 12 is {}", find_distance_to_closest_railroad(12));
+    println!("distance to closest railroad for 12 is {}", find_distance_to_closest_railroad(12));
+    println!("total steps for 12 is {}", find_distance_to_closest_railroad(12) + get_ring_number(12));
+    
+    println!("total steps for 23 is {}", find_distance_to_closest_railroad(23) + get_ring_number(23));
+    println!("total steps for 1024 is {}", find_distance_to_closest_railroad(1024) + get_ring_number(1024));
+    println!("total steps for 347991 is {}", find_distance_to_closest_railroad(347991) + get_ring_number(347991));
 }
 
 fn get_ring_number(num: usize) -> i64 {
   let square_root: f64 = (num as f64).sqrt();
-  // let lower_perfect = square_root as i64;
   let upper_perfect = square_root as i64 + 1;
 
   println!("upper perfect is {}", upper_perfect);
@@ -24,45 +24,66 @@ fn get_ring_number(num: usize) -> i64 {
   }
 }
 
-fn find_distance_to_closest_railroad(num: usize) -> usize {
+fn find_distance_to_closest_railroad(num: i64) -> i64 {
     let square_root: f64 = (num as f64).sqrt();
 
-    let upper_perfect: usize = (square_root as i64 + 1) as usize;
+    let upper_perfect: i64 = (square_root as i64 + 1) as i64;
     println!("upper perfect for {} is {}", num, upper_perfect);
 
-    let odd_perfect: usize;
+    let odd_perfect: i64;
     if upper_perfect % 2 != 0 {
         odd_perfect = upper_perfect;
     } else {
         odd_perfect = upper_perfect + 1;
     }
-    let length_of_side: usize = odd_perfect;
+    let length_of_side: i64 = odd_perfect;
     println!("odd perfect for {} is {}", num, odd_perfect);
 
-    let last_of_row: usize = odd_perfect * odd_perfect;
+    let last_of_row: i64 = odd_perfect * odd_perfect;
     let first_of_row = (odd_perfect - 2) * (odd_perfect - 2) + 1;
 
-    let railroad_4: usize = last_of_row - (length_of_side - 1)/ 2;
-    let railroad_3: usize = railroad_4 - length_of_side + 1;
-    let railroad_2: usize = railroad_3 - length_of_side + 1;
-    let railroad_1: usize = railroad_2 - length_of_side + 1;
+    let railroad_4: i64 = last_of_row - (length_of_side - 1)/ 2;
+    let railroad_3: i64 = railroad_4 - length_of_side + 1;
+    let railroad_2: i64 = railroad_3 - length_of_side + 1;
+    let railroad_1: i64 = railroad_2 - length_of_side + 1;
 
     // find which of the four railroads are closest to num
     if upper_perfect % 2 != 0{
-        if num - railroad_3 >= num - railroad_4 {
-            return num - railroad_4;
+        if (num - railroad_3).abs() >= (num - railroad_4).abs() {
+            return (num - railroad_4).abs();
         } else {
-            return num - railroad_3;
+            return (num - railroad_3).abs();
         }
     } else {
-        if num - railroad_1 >= num - railroad_2 {
-            return num - railroad_2;
+        println!("railroad 1 is {}, railroad 2 is {}, and num is {}", railroad_1, railroad_2, num);
+        if (num - railroad_1).abs() >= (num - railroad_2).abs() {
+            return (num - railroad_2).abs();
         } else {
-            return num - railroad_1;
+            return (num - railroad_1).abs();
         }
     }
 }
-// fn find_closest_square(num: usize) -> usize {
-// }
 
+#[test]
+fn can_find_ring_number() {
+    assert_eq!(get_ring_number(12), 2);
+    assert_eq!(get_ring_number(21), 2);
+    assert_eq!(get_ring_number(26), 3);
+    assert_eq!(get_ring_number(29), 3);
+}
 
+#[test]
+fn can_find_ring_number_of_perfect_squares() {
+    assert_eq!(get_ring_number(16), 2);
+    assert_eq!(get_ring_number(25), 2);
+}
+#[test]
+fn can_find_distance_to_closest_railroad() {
+    assert_eq!(find_distance_to_closest_railroad(12), 1);
+    assert_eq!(find_distance_to_closest_railroad(21), 2);
+}
+#[test]
+fn can_find_distance_to_closest_railroad_of_perfect_squares() {
+    assert_eq!(find_distance_to_closest_railroad(16), 1);
+    assert_eq!(find_distance_to_closest_railroad(25), 2);
+}
